8cceb58d838a0b60bd6730665c3a8f9ad506d794
Александр <0999353523ak@gmail.com>
2024-06-14 16:59:53 +0300

    make build

---
+++ index.8034792d.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".list-item__link"),t=document.getElementById("largeImg");e.forEach(function(e){e.addEventListener("click",function(e){e.preventDefault();var n=e.currentTarget;n!==t.src&&(t.src=n)})})});
+//# sourceMappingURL=index.8034792d.js.map
